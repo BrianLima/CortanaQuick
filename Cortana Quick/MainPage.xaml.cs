5565dac60977d4caa22ed70af700710d8c94b61e
Brian Lima <brian.ghwt@gmail.com>
2014-08-23 15:00:19 -0300

    Removing unnecessary comments generated by VS13.

--- Cortana Quick/MainPage.xaml.cs
+++ Cortana Quick/MainPage.xaml.cs
@@ -28,11 +28,6 @@ namespace Cortana_Quick
             // Set the page's ApplicationBar to a new instance of ApplicationBar.
             ApplicationBar = new ApplicationBar();
 
-            //// Create a new button and set the text value to the localized string from AppResources.
-            //ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-            //appBarButton.Text = AppResources.AppBarButtonText;
-            //ApplicationBar.Buttons.Add(appBarButton);
-
             // Create a new menu item with the localized string from AppResources.
             ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem("settings");
             appBarMenuItem.Click += AppBarMenuItem_Click;
