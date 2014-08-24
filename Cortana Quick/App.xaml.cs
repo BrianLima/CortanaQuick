e85522f59983e3f533f7ffa6ee2822e6564e2ec5
Brian Lima <brian.ghwt@gmail.com>
2014-08-23 21:25:08 -0300

    Added page transition effects

--- Cortana Quick/App.xaml.cs
+++ Cortana Quick/App.xaml.cs
@@ -120,7 +120,7 @@ namespace Cortana_Quick
 
             // Create the frame but don't set it as RootVisual yet; this allows the splash
             // screen to remain active until the application is ready to render.
-            RootFrame = new PhoneApplicationFrame();
+            RootFrame = new TransitionFrame();
             RootFrame.Navigated += CompleteInitializePhoneApplication;
 
             // Handle navigation failures
