a9150c39963476e3849c5f50f631fd47dd13d3c7
Brian Lima <Brian.ghwt@gmail.com>
2014-12-31 11:44:46 -0200

    Localized the Tutorial Page.
    Working to localize the app to various languages.

--- Cortana Quick/Resources/AppResources.Designer.cs
+++ Cortana Quick/Resources/AppResources.Designer.cs
@@ -79,7 +79,7 @@ namespace Cortana_Quick.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Cortana Quick.
+        ///   Looks up a localized string similar to Quick for Cortana.
         /// </summary>
         public static string ApplicationTitle {
             get {
@@ -138,6 +138,204 @@ namespace Cortana_Quick.Resources {
         public static string ResourceLanguage {
             get {
                 return ResourceManager.GetString("ResourceLanguage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Welcome to Quick, the first audible notes extension for Cortana. Here you can use Cortana to do quick audible notes! Let&apos;s go trough a fast how-to.
+        /// </summary>
+        public static string Tutorial1 {
+            get {
+                return ResourceManager.GetString("Tutorial1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to hello.
+        /// </summary>
+        public static string Tutorial1_Header {
+            get {
+                return ResourceManager.GetString("Tutorial1_Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Open Cortana by pressing the search button, or touching her live tile or, if your Windows Phone is updated with the Lumia Denim update, just say &apos;Hey Cortana&apos;..
+        /// </summary>
+        public static string Tutorial2 {
+            get {
+                return ResourceManager.GetString("Tutorial2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Now, say to her Quick + I + something you want to be remembered later:&quot;.
+        /// </summary>
+        public static string Tutorial2_1 {
+            get {
+                return ResourceManager.GetString("Tutorial2_1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Quick I Parked my car at sesame street.
+        /// </summary>
+        public static string Tutorial2_2 {
+            get {
+                return ResourceManager.GetString("Tutorial2_2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Quick I left my keys on my room.
+        /// </summary>
+        public static string Tutorial2_3 {
+            get {
+                return ResourceManager.GetString("Tutorial2_3", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Quick I put my torch on the dining table.
+        /// </summary>
+        public static string Tutorial2_4 {
+            get {
+                return ResourceManager.GetString("Tutorial2_4", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Quick I kept my .net book in the my bag.
+        /// </summary>
+        public static string Tutorial2_5 {
+            get {
+                return ResourceManager.GetString("Tutorial2_5", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to taking a note.
+        /// </summary>
+        public static string Tutorial2_Header {
+            get {
+                return ResourceManager.GetString("Tutorial2_Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Later, when you want to know something that you noted, open Cortana one more time and say to her:&quot;.
+        /// </summary>
+        public static string Tutorial3 {
+            get {
+                return ResourceManager.GetString("Tutorial3", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Quick where is my car?.
+        /// </summary>
+        public static string Tutorial3_1 {
+            get {
+                return ResourceManager.GetString("Tutorial3_1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Then, she will answear you with:.
+        /// </summary>
+        public static string Tutorial3_2 {
+            get {
+                return ResourceManager.GetString("Tutorial3_2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your car is at sesame street.
+        /// </summary>
+        public static string Tutorial3_3 {
+            get {
+                return ResourceManager.GetString("Tutorial3_3", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Now you are good to go! If you have any suggestions to improve this app, review it, i&apos;ll be glad to hear your opinion. Thanks!.
+        /// </summary>
+        public static string Tutorial3_4 {
+            get {
+                return ResourceManager.GetString("Tutorial3_4", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Quick where did i left my keys?.
+        /// </summary>
+        public static string Tutorial3_5 {
+            get {
+                return ResourceManager.GetString("Tutorial3_5", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Quick where did i put my torch?.
+        /// </summary>
+        public static string Tutorial3_6 {
+            get {
+                return ResourceManager.GetString("Tutorial3_6", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You left  your keys at your room.
+        /// </summary>
+        public static string Tutorial3_7 {
+            get {
+                return ResourceManager.GetString("Tutorial3_7", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your torch is on the dinning table.
+        /// </summary>
+        public static string Tutorial3_8 {
+            get {
+                return ResourceManager.GetString("Tutorial3_8", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to asking for a note.
+        /// </summary>
+        public static string Tutorial3_Header {
+            get {
+                return ResourceManager.GetString("Tutorial3_Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to If you don&apos;t know where Cortana is and your country native language is English, follow this guide to enable Cortana on your Windows Phone, If you live in Germany, Spain and others, wait a little bit, this app will support your language soon..
+        /// </summary>
+        public static string Tutorial4 {
+            get {
+                return ResourceManager.GetString("Tutorial4", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to How do i enable Cortana?.
+        /// </summary>
+        public static string Tutorial4_Button {
+            get {
+                return ResourceManager.GetString("Tutorial4_Button", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to where is she?.
+        /// </summary>
+        public static string Tutorial4_Header {
+            get {
+                return ResourceManager.GetString("Tutorial4_Header", resourceCulture);
             }
         }
         
